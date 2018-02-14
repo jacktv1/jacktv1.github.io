73f208f868710a17d9ba1997ba9795353a40e1e3
Ngo Quoc Anh <ngoquocanh111@gmail.com>
2018-02-14 15:31:00 +0700

    end-no-edit

--- jquery/form-validate/js/app.js
+++ jquery/form-validate/js/app.js
@@ -17,10 +17,10 @@ $(document).ready(function() {
 		var $email = $('#email');
 		var $birthday = $('#birthday');
 
-		var $validUsername = validate.checkUsername($username.val());
-		var $validPassword = validate.checkPassword($password.val());
-		var $validEmail = validate.checkEmail($email.val());
-		var $validBirthday = validate.checkBirthday($birthday.val());
+		// var $validUsername = validate.checkUsername($username.val());
+		// var $validPassword = validate.checkPassword($password.val());
+		// var $validEmail = validate.checkEmail($email.val());
+		// var $validBirthday = validate.checkBirthday($birthday.val());
 
 		// If validate with javascript is passed then validate with php through fetch api
 		// if ($validUsername && $validPassword && $validEmail && $validBirthday) {
